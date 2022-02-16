a9a1f4b3f17fac7c54981625a17011279ac29748
kristiyan2021 <449302@student.saxion.nl>
2022-02-16 13:13:21 +0100

    feat: connection and disconnection handlers

--- multiplayer-game/index.js
+++ multiplayer-game/index.js
@@ -1,11 +1,26 @@
-const express = require("express")
+const PORT = 3000;
 
-var app = express()
-
-app.get("/",function(request,response){
-
-response.send("Hello World!")
-})
-app.listen(3000, function () {
-console.log("Started application on port %d", 3000)
+const express = require('express');
+const app = express();
+const httpServer = app.listen(PORT, function () {
+    console.log(`Started application on port ${PORT}`);
 });
+const io = require('socket.io')(httpServer);
+
+let connections = [];
+
+io.on('connection', function (socket) {
+    handleConnect(socket);
+
+    socket.on('disconnect', () => handleDisconnect(socket));
+});
+
+function handleConnect(socket) {
+    connections.push(socket.id);
+    console.log(`Established connection with client ${socket.id}`);
+}
+
+function handleDisconnect(socket) {
+    connections = connections.filter(client => client !== socket.id);
+    console.log(`Client ${socket.id} has disconnected!`);
+}
