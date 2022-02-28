bc586275fb72d9888ed9e2f869fc0ea5e54b2927
manhphim1205 <ngocminhpham66@gmail.com>
2022-02-28 10:07:31 +0100

    feat: lobby menu UI

--- game/frontend/src/main.js
+++ game/frontend/src/main.js
@@ -1,9 +1,15 @@
+/** @type {import("../typings/phaser")} */
 import Phaser from 'phaser';
 import TitleScreen from './scenes/TitleScreen';
+import { LoadScene } from './scenes/LoadScene';
+import { MenuScene } from './scenes/MenuScene' 
 
 const config = {
     width: 1280,
     height: 720,
+    scene: [
+        LoadScene, MenuScene
+    ],
     parent: "game",
     type: Phaser.AUTO,
     dom: {
@@ -12,7 +18,7 @@ const config = {
 }
 
 const game = new Phaser.Game(config);
-game.scene.add('titlescreen', TitleScreen);
+// game.scene.add('titlescreen', TitleScreen);
 
-game.scene.start('titlescreen');
+// game.scene.start('titlescreen');
 
---
+++ game/frontend/src/scenes/LoadScene.js
@@ -0,0 +1,40 @@
+import { CST } from "../CST";
+
+export class LoadScene extends Phaser.Scene {
+    constructor() {
+        super({
+            key: CST.SCENES.LOAD
+        })
+    }
+    init() {
+
+    }
+    preload() {
+        this.load.image("title_bg", "assets/image/title_bg.jpg");
+        this.load.image("options_button", "assets/image/option.png");
+        this.load.image("play_button", "assets/image/create_room.png");
+        this.load.image("join_button", "assets/image/join_room.png")
+        this.load.image("logo", "assets/image/logo_fix.png");
+
+        let loadingBar = this.add.graphics({
+            fillStyle: {
+                color: 0xFFFF
+            }
+        })
+        this.load.on("progress", (percent)=> {
+            loadingBar.fillRect(0, this.game.renderer.height / 2, this.game.renderer.width * percent, 50);
+            this.add.text(this.game.renderer.width / 2 - 100, this.game.renderer.height / 2 - 100, 'Loading..', 
+            { 
+                fontFamily: 'Arial', fontSize: 64, color: '#fff'
+            });
+            console.log(percent)
+        })
+
+        this.load.on("complete", () => {
+            console.log("done")
+        })
+    }
+    create() {
+        this.scene.start(CST.SCENES.MENU)
+    }
+}
