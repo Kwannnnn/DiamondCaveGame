efacc6db99c91402b1757d65303bdee01764e339
manhphim1205 <ngocminhpham66@gmail.com>
2022-03-10 15:54:42 +0100

    feat: create chat interface

--- game/frontend/src/scenes/Game.js
+++ game/frontend/src/scenes/Game.js
@@ -1,5 +1,5 @@
 import Phaser from 'phaser';
-import { CST } from '../CST';
+import { CST } from '../utils/CST';
 
 import DiamondCollectEventHandler from '../events/CollectDiamondEvent';
 import HUD from './HUD';
