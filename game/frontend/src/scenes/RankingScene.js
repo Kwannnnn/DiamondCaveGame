971ae60525e1e13a81c690a36eefcd3c36000ccb
kristiyan2021 <449302@student.saxion.nl>
2022-03-15 19:34:12 +0100

    feat(frontend): Header component

--- game/frontend/src/scenes/RankingScene.js
+++ game/frontend/src/scenes/RankingScene.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import { CST } from '../utils/CST';
+import { Header } from '../components';
 
 export default class RankingScene extends Phaser.Scene {
     constructor() {
@@ -13,6 +14,9 @@ export default class RankingScene extends Phaser.Scene {
     }
 
     create() {
+        this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.25, 'logo').setDepth(1);
+        this.add.image(0,0, 'title_bg').setOrigin(0).setDepth(0);
 
+        var header = new Header(this, this.game.renderer.width / 2, this.game.renderer.height / 2, 'Top 10 runs');
     }
 }
