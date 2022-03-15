1df053342192b5723ad4a1746ff6805ab79936bf
kristiyan2021 <449302@student.saxion.nl>
2022-03-15 19:18:29 +0100

    chore: Optimized imports for components

--- game/frontend/src/scenes/MenuScene.js
+++ game/frontend/src/scenes/MenuScene.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser';
 import { CST } from "../utils/CST";
-import MenuButton from '../view/MenuButton';
+import { MenuButton } from '../components';
 
 export default class MenuScene extends Phaser.Scene {
     constructor() {
@@ -9,18 +9,13 @@ export default class MenuScene extends Phaser.Scene {
         })
     }
 
-    init(data) {
-        
-    }
-
     preload() {
         this.load.image("back", "assets/arrow-left.png");
-
     }
 
     create() {
         this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.25, "logo").setDepth(1);
-        this.add.image(0,0, "title_bg").setOrigin(0).setDepth(0)
+        this.add.image(0,0, "title_bg").setOrigin(0).setDepth(0);
         this.createNewRoomButton = new MenuButton(this, this.game.renderer.width / 2, this.game.renderer.height - 350, "play_button", CST.SCENES.LOBBY);
         this.joinRoomButton = new MenuButton(this, this.game.renderer.width / 2, this.game.renderer.height - 250, "join_button", CST.SCENES.JOIN);
         this.rankingButton = new MenuButton(this, this.game.renderer.width / 2, this.game.renderer.height - 150, "options_button", CST.SCENES.RANKING);
