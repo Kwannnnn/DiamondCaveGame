7586da88f48ebaa50257f117684e50af3ea2924e
kristiyan2021 <449302@student.saxion.nl>
2022-03-16 10:31:12 +0100

    feat(frontend): Basic table container

--- game/frontend/src/utils/CST.js
+++ game/frontend/src/utils/CST.js
@@ -8,5 +8,10 @@ export const CST = {
         ACTIVEGAMES: "ACTIVEGAMES",
         SPECTATORJOIN: "SPECTATORJOIN",
         RANKING: "RANKING"
+    },
+    COLORS: {
+        RANKLIST_PRIMARY: 0x4e342e,
+        RANKLIST_SECONDARY: 0x260e04,
+        RANKLIST_TERNARY: 0x7b5e57
     }
 }
