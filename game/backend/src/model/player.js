d0241ba9f2c1b05a3000baca2ed15cd612c72342
kristiyan2021 <449302@student.saxion.nl>
2022-03-30 21:09:16 +0200

    feat(backend): update Player model - username

--- game/backend/src/model/player.js
+++ game/backend/src/model/player.js
@@ -2,6 +2,11 @@ class Player {
     constructor(id, socket) {
         this.id = id;
         this.socket = socket;
+        this.username = undefined;
+    }
+
+    setUsername(username) {
+        this.username = username;
     }
 }
 
